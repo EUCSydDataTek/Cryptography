af3d621c01e0471ade7ed8206aa84b21fa246a0c
EgonRasmussen <ecr@eucsyd.dk>
2022-08-03 18:00:08 +0200

    DigitalSignature changed

--- 8. DigitalSignature/Program.cs
+++ 8. DigitalSignature/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static void Main()
     {
-        byte[] document = Encoding.UTF8.GetBytes("Document to Sign");
+        byte[] document = Encoding.UTF8.GetBytes("Du har betalt din regning");
 
         using SHA256 sha256 = SHA256.Create();
         byte[] hashedDocument = sha256.ComputeHash(document);
@@ -15,6 +15,11 @@ class Program
         digitalSignature.AssignNewKey();
 
         byte[] signature = digitalSignature.SignData(hashedDocument);
+
+             // Følgende to linjer simulerer at dokumentet er blevet ændret undervejs
+        //document = Encoding.UTF8.GetBytes("Du har ikke betalt din regning");
+        //hashedDocument = sha256.ComputeHash(document);
+
         bool verified = digitalSignature.VerifySignature(hashedDocument, signature);
 
         Console.WriteLine("Digital Signature Demonstration in .NET");
